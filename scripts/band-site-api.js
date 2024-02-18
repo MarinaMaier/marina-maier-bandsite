ba9943f59dc32b51bafd8cdf5666bb55504f53e7
Marina Maier <marina.maier.2023@gmail.com>
2024-02-17 23:26:27 -0500

    set new instances, fix comments in comments form

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -3,42 +3,42 @@ const baseUrl = 'https://unit-2-project-api-25c1595833b2.herokuapp.com';
 export async function getApiKey() {
     try {
         const resp = await axios.get(`${baseUrl}/register`);
-        return resp.data.api_key; 
+        return resp.data.api_key;
     } catch (error) {
         console.error('Failed to get API Key: ', error);
         throw error;
     }
 }
 export class BandSiteApi {
-    apiKey; 
-
+    apiKey;
     constructor(apiKey) {
-        this.apiKey = apiKey; 
+        this.apiKey = apiKey;
     }
-
     async postComment(comment) {
         try {
+            const headers = {
+                'Content-Type': 'application/json',
+            };
             const apiUrl = `${baseUrl}/comments?api_key=${this.apiKey}`;
-            const response = await axios.post(apiUrl, comment);
+            const requestBody = comment;
+            const response = await axios.post(apiUrl, requestBody, { headers });
             return response.data;
         } catch (error) {
             console.error('Failed posting comment: ', error);
             throw error;
         }
     }
-
     async getComments() {
         try {
             const apiUrl = `${baseUrl}/comments?api_key=${this.apiKey}`;
             const response = await axios.get(apiUrl);
-            const sortedComments = response.data.comments.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-            return sortedComments;
+            return response.data;
+            console.log(response);
         } catch (error) {
             console.error('Failed getting comments: ', error);
             throw error;
         }
     }
-
     async getShows() {
         try {
             const apiUrl = `${baseUrl}/showdates?api_key=${this.apiKey}`;
